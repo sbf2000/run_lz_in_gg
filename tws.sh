ef1fb3f6d360e89df27b425d9ff8631c1d04ffff
sbf2000 <sbf2000@foxmail.com>
2018-09-24 00:01:35 +0800

    Update for Colab

---
+++ tws.sh
@@ -0,0 +1,22 @@
+ 
+#!/bin/sh 
+#===================== 
+#YuanHui.HE 
+#khler@163.com 
+#===================== 
+while : 
+do 
+echo "Current DIR is " $PWD 
+stillRunning=$(ps -ef |grep "$PWD/autogtp" |grep -v "grep") 
+if [ "$stillRunning" ] ; then 
+echo "TWS service was already started by another way" 
+echo "Kill it and then startup by this shell, other wise this shell will loop out this message annoyingly" 
+kill -9 $pidof $PWD/loader 
+else 
+echo "TWS service was not started" 
+echo "Starting service ..." 
+$PWD/autogtp -g 8 
+echo "TWS service was exited!" 
+fi 
+sleep 10 
+done 
